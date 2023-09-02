1d64f1064f96c074642323d5dd1c69965fb5e8c3
“Itopan1108” <tito@esm.co.jp>
2023-09-02 11:18:45 +0900

    2-2対応レビュー指摘取り込み後１

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ function doGet(e) {
   const ss = SpreadsheetApp.openById('10GkWRFS8PaaOk8r0YBU7P5xu4oyLSpUJH196g5j-REs');
   const sheet = ss.getSheetByName('シート1');
   const range = sheet.getRange('B2');
-      // １つ以上のセル（今回は１つのセル）の値を取得
+  // １つ以上のセル（今回は１つのセル）の値を取得
   const values = range.getValues();
-  template.values = values; // シートから取得した値を画面に渡します
+  // シートから取得した値を画面に渡す
+  template.values = values; 
   return template.evaluate();
 }
 
