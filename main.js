b6b9a098823df926955590462d566b0e4aa0d870
“Itopan1108” <tito@esm.co.jp>
2023-10-19 22:27:28 +0900

    4-5,6 指摘た対応（残あり）

--- main.js
+++ main.js
@@ -23,3 +23,39 @@ function doGet() {
    const range = sheet.getRange('B2')
    range.setValue(Value);
  }
+
+
+ //（４－６ DBから値を取得する関数）
+
+function readFromTable() {
+  const connection = Jdbc.getCloudSqlConnection(url, userName, password);
+  const statement = connection.createStatement();
+  const results = statement.executeQuery('SELECT * FROM countup');
+ 
+  var  count_number = results.getInt('count_number');
+  Logger.log(count_number);
+  return count_number;   
+
+  results.close();
+  statement.close();
+  connection.close();
+}
+
+//（４－５ DBに値を更新する関数）
+
+function updateRecord(count_number) {
+  const connection = Jdbc.getCloudSqlConnection(url, userName, password);
+
+  const statement = connection.prepareStatement('INSERT INTO users (id, count_number) values (?, ?)  ON DUPLICATE KEY UPDATE id = ?');
+  statement.setInt(1, 1);
+  statement.setInt(2, count_number + 1);
+  statement.setInt(3, 1);
+
+  const row = statement.executeUpdate();
+  Logger.log(row);
+
+  connection.commit();
+  statement.close();
+  connection.close();
+}
+
